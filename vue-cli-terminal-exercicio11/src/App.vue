d06b5594b5bdc1e248eeb55baa2604c4a2b2073f
Everton Cabral <evertoncab@gmail.com>
2021-02-18 12:23:00 -0300

    criação do componente ListenItem.vue

--- vue-cli-terminal-exercicio11/src/App.vue
+++ vue-cli-terminal-exercicio11/src/App.vue
@@ -1,18 +1,30 @@
 <template>
   <div id="app">
+  
+  
+  
   </div>
 </template>
 
 <script>
+import ListenItem from "./ListenItem.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-  }
-}
+    ListenItem,
+  },
+  data() {
+    return {};
+  },
+};
 </script>
 
 <style>
+*{
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
